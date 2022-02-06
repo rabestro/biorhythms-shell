1af63a0271fba20f432f3149214cbd24d40271e8
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-06 22:35:19 +0200

    Initial commit

---
+++ src/main/java/lv/id/jc/biorhythms/cli/BiorhythmsShellDraftApplication.java
@@ -0,0 +1,13 @@
+package lv.id.jc.biorhythms.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiorhythmsShellDraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiorhythmsShellDraftApplication.class, args);
+    }
+
+}
