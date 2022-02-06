a7b878cc0fb44de49922230bd25c2459aae0ade9
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-07 00:13:01 +0200

    Add prompt provider

---
+++ src/main/java/lv/id/jc/biorhythms/cli/shell/Prompt.java
@@ -0,0 +1,17 @@
+package lv.id.jc.biorhythms.cli.shell;
+
+import lv.id.jc.biorhythms.cli.model.Context;
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public record Prompt(Context context) implements PromptProvider {
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString(context().getDate().toString() + ":>",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
+    }
+}
