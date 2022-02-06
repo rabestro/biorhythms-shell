1d821cb6a21e1a938d0c368f7108d0d632d0d844
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-07 00:25:25 +0200

    Add PrettyPeriodFormat

--- src/main/java/lv/id/jc/biorhythms/cli/shell/BiorhythmsCalculatorCommands.java
+++ src/main/java/lv/id/jc/biorhythms/cli/shell/BiorhythmsCalculatorCommands.java
@@ -1,13 +1,23 @@
 package lv.id.jc.biorhythms.cli.shell;
 
+import lv.id.jc.biorhythms.cli.format.PrettyPeriodFormat;
 import lv.id.jc.biorhythms.cli.model.Context;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 @ShellComponent
 public record BiorhythmsCalculatorCommands(Context context) {
+    private static final String AGE_REPORT = """
+              Age Report%n
+              Birthday: %tA, %<tB %<td, %<tY
+                 Today: %tA, %<tB %<td, %<tY
+                  Days: %,d
+                   Age: %s
+            """;
 
     @ShellMethod(value = "set birthday of the person", key = {"birthday", "bd"})
     public LocalDate birthday(LocalDate birthday) {
@@ -15,8 +25,13 @@ public record BiorhythmsCalculatorCommands(Context context) {
         return birthday;
     }
 
-    @ShellMethod
+    @ShellMethod(value = "prints age info")
     public String ageInfo() {
-        return null;
+        return String.format(AGE_REPORT,
+                context.getBirthday(),
+                context().getDate(),
+                ChronoUnit.DAYS.between(context.getBirthday(), context().getDate()),
+                PrettyPeriodFormat.getInstance().format(Period.between(context.getBirthday(), context().getDate()))
+        );
     }
 }
